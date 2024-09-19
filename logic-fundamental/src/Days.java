4aaf590bda33e9c6762cb293776a82d85598df78
Raynaldi Faizal Biqri <hellobiqri@gmail.com>
2024-09-19 11:04:32 +0700

    -fixed Days

--- logic-fundamental/src/Days.java
+++ logic-fundamental/src/Days.java
@@ -13,10 +13,10 @@ public class Days {
 
     public static void whatDay(String day) {
         int resultNumber = DayNumber.findDayNumber(day);
-        String kemarinLusa = DayName.findDayName(resultNumber - 2);
+        String kemarinLusa = DayName.findDayName(resultNumber + 5);
         System.out.println("Kemarin lusa adalah hari " + kemarinLusa + " (2 hari sebelum) " + day);
 
-        String kemarin = DayName.findDayName(resultNumber - 1);
+        String kemarin = DayName.findDayName(resultNumber + 6);
         System.out.println("Kemarin adalah hari " + kemarin + " (hari sebelum) " + day);
 
         System.out.println("Hari ini adalah " + day + " (didapat dari) " + day);
