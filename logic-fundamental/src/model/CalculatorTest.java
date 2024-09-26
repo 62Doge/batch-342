46ca7f0665eb22a9374421809000655db486a35d
Raynaldi Faizal Biqri <hellobiqri@gmail.com>
2024-09-26 10:05:35 +0700

    -add calculator1 object

--- logic-fundamental/src/model/CalculatorTest.java
+++ logic-fundamental/src/model/CalculatorTest.java
@@ -5,6 +5,7 @@ public class CalculatorTest {
         // Using parameter Constructor
         Calculator calculator = new Calculator(5, 5);
 
+        System.out.println("Using empty constructor");
         System.out.println(calculator.sum());
         System.out.println(calculator.minus());
         System.out.println(calculator.divide());
@@ -15,9 +16,10 @@ public class CalculatorTest {
         calculator1.setNumber1(9);
         calculator1.setNumber2(5);
 
-        System.out.println(calculator.sum());
-        System.out.println(calculator.minus());
-        System.out.println(calculator.divide());
-        System.out.println(calculator.multiply());
+        System.out.println("Using parameter constructor");
+        System.out.println(calculator1.sum());
+        System.out.println(calculator1.minus());
+        System.out.println(calculator1.divide());
+        System.out.println(calculator1.multiply());
     }
 }
