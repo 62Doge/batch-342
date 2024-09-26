98e063300b4736646c24813d673073069c549795
Raynaldi Faizal Biqri <hellobiqri@gmail.com>
2024-09-26 10:12:24 +0700

    -implement getter

--- logic-fundamental/src/model/CalculatorTest.java
+++ logic-fundamental/src/model/CalculatorTest.java
@@ -6,17 +6,22 @@ public class CalculatorTest {
         Calculator calculator = new Calculator(5, 5);
 
         System.out.println("Using empty constructor");
+        System.out.println("Number 1: " + calculator.getNumber1());
+        System.out.println("Number 2: " + calculator.getNumber2());
         System.out.println(calculator.sum());
         System.out.println(calculator.minus());
         System.out.println(calculator.divide());
         System.out.println(calculator.multiply());
 
+        System.out.println();
         // Using empty Constructor
         Calculator calculator1 = new Calculator();
         calculator1.setNumber1(9);
         calculator1.setNumber2(5);
 
         System.out.println("Using parameter constructor");
+        System.out.println("Number 1: " + calculator1.getNumber1());
+        System.out.println("Number 2: " + calculator1.getNumber2());
         System.out.println(calculator1.sum());
         System.out.println(calculator1.minus());
         System.out.println(calculator1.divide());
